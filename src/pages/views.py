74473ea984c19962f6480f6ad44d71c6f625f8aa
kendrick010 <nguyen.kendrick@yahoo.com>
2024-07-23 22:05:44 -0500

    templating

--- src/pages/views.py
+++ src/pages/views.py
@@ -3,13 +3,22 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
-    return HttpResponse("<h1>Hello World</h1>")
+    print(args, kwargs)
+    print(request.user)
+
+    return render(request=request, template_name="home.html", context={})
 
 def contact_view(request, *args, **kwargs):
-    return HttpResponse("<h1>Contact Page</h1>")
+    return render(request=request, template_name="contact.html", context={})
 
 def about_view(request, *args, **kwargs):
-    return HttpResponse("<h1>About Page</h1>")
+    my_context = {
+        "my_text": "This is about us",
+        "my_number": 123,
+        "my_list": [123, 456, 789],
+    }
+
+    return render(request=request, template_name="about.html", context=my_context)
 
 def social_view(request, *args, **kwargs):
-    return HttpResponse("<h1>Social Page</h1>")
+    return render(request=request, template_name="social.html", context={})
