b12a84c5af2a73c14c6b53c1a4fb2c70390da0f6
kendrick010 <nguyen.kendrick@yahoo.com>
2024-07-26 22:19:19 -0500

    forms

--- src/django_scratch/urls.py
+++ src/django_scratch/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path
 
 from pages.views import home_view, contact_view, about_view, social_view
-from products.views import product_detail_view
+from products.views import product_detail_view, product_create_view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -28,5 +28,6 @@ urlpatterns = [
     path('contact/', contact_view, name='contact'),
     path('about/', about_view, name='about'),
     path('social/', social_view, name='social'),
-    path('product/', product_detail_view, name='product')
+    path('product/', product_detail_view, name='product'),
+    path('create/', product_create_view, name='create'),
 ]
--- src/products/views.py
+++ src/products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Product
+from .forms import ProductCreateForm
 
 # Create your views here.
 def product_detail_view(request):
@@ -9,3 +10,26 @@ def product_detail_view(request):
     }
 
     return render(request=request, template_name="product/detail.html", context=context)
+
+# def product_create_view(request):
+#     form = ProductCreateForm(request.POST or None)
+#     if form.is_valid():
+#         form.save()
+#         form = ProductCreateForm()
+
+#     context = {
+#         "form": form,
+#     }
+
+#     return render(request=request, template_name="product/create.html", context=context)
+
+def product_create_view(request):
+    print(request.GET)
+    print(request.POST)
+    
+    if request.method == "POST":
+        title = request.POST.get('title')
+        print(title)
+
+    context = {}
+    return render(request=request, template_name="product/create.html", context=context)
